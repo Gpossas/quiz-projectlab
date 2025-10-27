4c9b018faf3541fb33d1117540bebbf1e18bc8e5
gpossas <possasg@gmail.com>
2025-10-26 23:22:39 -0300

    remove unused import

--- src/main/java/com/api/quizAI/web/dto/RoomRequestDTO.java
+++ src/main/java/com/api/quizAI/web/dto/RoomRequestDTO.java
@@ -1,6 +1,5 @@
 package com.api.quizAI.web.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
